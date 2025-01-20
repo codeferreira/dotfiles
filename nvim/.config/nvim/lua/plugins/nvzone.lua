763663cfd7b2f5014b182cf1082cb2aae1a8f75a
José Ferreira <dev.josejunior@gmail.com>
2025-01-20 13:46:40 -0300

    update nvim config

---
+++ nvim/.config/nvim/lua/plugins/nvzone.lua
@@ -0,0 +1,17 @@
+return {
+  { "nvzone/volt", lazy = true },
+  { "nvzone/menu", lazy = true },
+  {
+    "nvzone/timerly",
+    cmd = "TimerlyToggle",
+    keys = {
+      {
+        "<leader>uT",
+        "<cmd> TimerlyToggle <CR>",
+        desc = "Opens timerly",
+      },
+    },
+  },
+  { "nvzone/showkeys", cmd = "ShowkeysToggle" },
+  { "nvzone/typr", cmd = "TyprToggle" },
+}
