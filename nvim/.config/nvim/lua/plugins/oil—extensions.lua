be7254b4ba95368401a97298193c87ed81119c49
José Ferreira <dev.josejunior@gmail.com>
2025-09-19 23:08:01 -0300

    update configs

---
+++ nvim/.config/nvim/lua/plugins/oil—extensions.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "refractalize/oil-git-status.nvim",
+    dependencies = {
+      "stevearc/oil.nvim",
+    },
+    config = true,
+  },
+  {
+    "JezerM/oil-lsp-diagnostics.nvim",
+    dependencies = { "stevearc/oil.nvim" },
+    opts = {},
+  },
+}
