067ddc2ab91c885993a578efb177352a2be02870
José Ferreguetti <jferreguetti@paypal.com>
2025-10-21 14:56:05 -0300

    update configs

--- nvim/.config/nvim/lua/plugins/oil-extensions.lua
+++ nvim/.config/nvim/lua/plugins/oil-extensions.lua
@@ -1,7 +1,7 @@
 return {
-  {
-    "benomahony/oil-git.nvim",
-    dependencies = { "stevearc/oil.nvim" },
-    -- No opts or config needed! Works automatically
-  },
+  -- {
+  --   "benomahony/oil-git.nvim",
+  --   dependencies = { "stevearc/oil.nvim" },
+  --   -- No opts or config needed! Works automatically
+  -- },
 }
